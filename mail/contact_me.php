e24ac15783b1b2edd23d4d096189340cebd5d9c8
Alireza Omidi <alireza530@gmail.com>
2015-08-29 17:03:32 +0430

    added register php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,64 @@
 <?php
+
+function generateRandomString($length = 15) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+$servername = 'localhost';
+$username = 'fosscon';
+$password = 'sefri92';
+$dbname = 'fosscon';
+
+$con = new mysqli($servername, $username, $password, $dbname);
+if ($con->connect_error) {
+    die('Connection failed! ' . $con->connect_error);
+}
+
+
 // Check for empty fields
-if(empty($_POST['name'])  		||
+if(empty($_POST['name'])        ||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
+   empty($_POST['stdnum'])      ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
-	return false;
+       echo "No arguments Provided!";
+       return false;
    }
-	
+
 $name = $_POST['name'];
-$email_address = $_POST['email'];
+$email = $_POST['email'];
 $phone = $_POST['phone'];
-$message = $_POST['message'];
-	
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+$stdnum = $_POST['stdnum'];
+$uniqid = generateRandomString();
+
+$query = "select * from users where email='".$email."'";
+if ($con->query($query)->num_rows >= 1) {
+    die('Email duplicate.');
+}
+
+while(true){
+    $query = "select * from users where uniqid='".$uniqid."'";
+    if ($con->query($query)->num_rows == 0) {
+        break;
+    }
+    $uniqid = generateRandomString();
+}
+
+$query = sprintf('insert into users (name, email, phone, stdnum, uniqid) values ("%s", "%s", "%s", "%s", "%s")', $name, $email, $phone, $stdnum, $uniqid);
+
+if ($con->query($query) === true) {
+    echo 'query succeed';
+} else {
+    echo "ERROR: " . $con->error;
+}
+
+$con->close();
+
 ?>
