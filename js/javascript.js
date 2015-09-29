3b563050bc4014d5df3f1fd5a10422a7aa628412
professormahi_f@yahoo.com <professormahi_f@yahoo.com>
2015-09-29 17:56:02 +0330

    metaco :D

--- js/javascript.js
+++ js/javascript.js
@@ -12,3 +12,18 @@ function frame_builder() {
     // Check function
     // $('#test').text('test');
 };
+
+function submit_click() {
+  var detail = $('#workshopsForm').serialize();
+  var index = detail.indexOf('workshop=');
+  if (index === -1){
+  } else {
+    index = index + 'workshop='.length;
+    var _index = detail.indexOf('&');
+    var workshop = detail.substring(index, _index);
+    var code = detail.substring(detail.indexOf('code=') + 'code='.length);
+    var url = "http://myevent.ir/IUTFOSSCon/register?quantity[" + workshop + "]=1&discount_code=" + code;
+    // window.location.replace(url);
+    window.open(url);
+  }
+}
